7c17347cf4cff9693376c1409761e48ef30cb7dd
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-05-04 19:49:51 +0700

    first app flutter

---
+++ first_app/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("my first app"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: const Center(
+        child: Image(
+          image: AssetImage("assets/nino-1.jfif"),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Text("+"),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}

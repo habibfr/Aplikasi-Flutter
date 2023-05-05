d5f9e32cd6d0f3dd25403bb5b47673c17d3aa5a6
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-05-05 16:37:54 +0700

    list data

--- quotes_project/lib/main.dart
+++ quotes_project/lib/main.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(
+    MaterialApp(
+      home: QuotesList(),
+    )
+  );
+}
+
+
+class QuotesList extends StatefulWidget {
+  const QuotesList({Key? key}) : super(key: key);
+
+  @override
+  State<QuotesList> createState() => _QuotesListState();
+}
+
+class _QuotesListState extends State<QuotesList> {
+
+  List<String> quotes = [
+    'hello my name is habib',
+    'belajar flutter di net ninja',
+    'sekarang adalah tahun 2023 bulan mei'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Quotes Awasome"),
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: quotes.map((quote) => Text(quote)).toList(),
+      ),
+    );
+  }
 }

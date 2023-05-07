fe157d95ed72bcc0d2cf81016bef8ebe7b4b335b
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-05-07 13:08:09 +0700

    Streams

--- brew_project/lib/screens/services/auth.dart
+++ brew_project/lib/screens/services/auth.dart
@@ -2,24 +2,29 @@ import 'package:brew_project/models/my_user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   // create user based on uid
-  MyUser? _userFromFirebaseUser(User user){
+  MyUser? _userFromFirebaseUser(User user) {
     return user != null ? MyUser(uid: user.uid) : null;
+  }
+
+  Stream<MyUser?> get user {
+    return _auth
+        .authStateChanges()
+        .map((User? user) => _userFromFirebaseUser(user!));
+    // .map(_userFromFirebaseUser);
   }
 
   // sign anonim
   Future signInAnonim() async {
-    try{
+    try {
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
       return _userFromFirebaseUser(user!);
-    }catch(e){
+    } catch (e) {
       print(e.toString());
       return null;
     }
   }
-
 }

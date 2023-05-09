3502e9f152ae7608c8f936765e7f546317c2f742
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-05-09 22:13:54 +0700

    user data model

--- brew_project/lib/models/my_user.dart
+++ brew_project/lib/models/my_user.dart
@@ -5,3 +5,16 @@ class MyUser {
 
   MyUser({this.uid});
 }
+
+class UserData {
+  final String uid;
+  final String name;
+  final int strength;
+  final String sugars;
+
+  UserData(
+      {required this.uid,
+      required this.name,
+      required this.strength,
+      required this.sugars});
+}
--- brew_project/lib/screens/services/database.dart
+++ brew_project/lib/screens/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:brew_project/models/brew.dart';
+import 'package:brew_project/models/my_user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
@@ -24,10 +25,22 @@ class DatabaseService {
     }).toList();
   }
 
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(
+        uid: uid!,
+        name: snapshot['name'],
+        strength: snapshot['strength'],
+        sugars: snapshot['sugars']);
+  }
+
   Stream<List<Brew>> get brews {
     return brewCollection
         .snapshots()
         // .map((QuerySnapshot snapshot) => _brewListFromSnapshot(snapshot));
         .map((_brewListFromSnapshot));
+  }
+
+  Stream<UserData> get userData {
+    return brewCollection.doc(uid).snapshots().map(_userDataFromSnapshot);
   }
 }
